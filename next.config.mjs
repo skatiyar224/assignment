cc1cf9823e7ee6d6b94c3d134c2a8e1cd7f4b055
shivam Katiyar <skatiyar224@gmail.com>
2024-09-04 11:30:46 -0700

    Deploy Next.js app to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-};
-
-export default nextConfig;
+    trailingSlash: true, // Optional: Adds trailing slashes to URLs
+    basePath: '/assignment', // Replace with your repository name if it's a project page
+    assetPrefix: '/assignment/', // Also replace if applicable
+  };
+  
+  export default nextConfig;
+  
